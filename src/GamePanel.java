81426b16423edf9a447c62e5f22b3793d715c745
Vinh Nguyen <vinh29012005@gmail.com>
2024-05-11 23:55:06 -0500

    modified

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -63,8 +63,10 @@ public class GamePanel extends JPanel implements ActionListener{
     public void move(){
         for (int i = bodyParts; i > 0; i--){
             x[i] = x[i - 1];
-            
+            y[i] = y[i - 1];
         }
+
+        
     }  
     
     public void checkApple(){
