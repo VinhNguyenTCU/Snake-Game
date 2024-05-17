38d22383f9eac7837c916218ec61e25eb4f3d90b
Vinh Nguyen <vinh29012005@gmail.com>
2024-05-17 08:23:26 +0700

    direction method for each direction

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -52,6 +52,16 @@ public class GamePanel extends JPanel implements ActionListener{
 
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++){
+            if (i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }else{
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple(){
@@ -66,6 +76,24 @@ public class GamePanel extends JPanel implements ActionListener{
             y[i] = y[i - 1];
         }
 
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+            
+            default:
+                break;
+        }
+
         
     }  
     
@@ -74,7 +102,13 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void checkCollisions(){
-
+        // check if head collides with body
+        for(int i = bodyParts; i > 0; i--){
+            if((x[0] == x[i]) && (y[0] == y[i])){
+                running = false;
+            }
+        }
+        // check if head touches left border
     }
 
     public void gameOver(Graphics g){
@@ -83,7 +117,12 @@ public class GamePanel extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e){
-
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter{
